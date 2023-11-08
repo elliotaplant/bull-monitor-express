b5a19361f154fe82ac99b7266233007af24eaf1b
Elliot Plant <elliotaplant@gmail.com>
2023-11-08 10:20:57 -0800

    Get bull monitor working!

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import { BullMonitorExpress } from '@bull-monitor/express';
-import Express from 'express';
+import Queue from 'bull';
+import express from 'express';
+import { Redis } from 'ioredis';
+import { BullAdapter } from '@bull-monitor/root/dist/bull-adapter';
 
-const port = 3000;
-const baseUrl = '/some/nested/url';
+const redis = new Redis(process.env.REDIS_URL || '');
+
+const port = 4444;
+const baseUrl = '/ui';
 (async () => {
-  const app = Express();
-  const monitor = new BullMonitorExpress({ queues: [] });
+  const app = express();
+  const monitor = new BullMonitorExpress({
+    queues: [new BullAdapter(new Queue('extraction', { createClient: () => redis }))],
+  });
   await monitor.init();
   app.use(baseUrl, monitor.router);
   app.listen(port, () => console.log(`http://localhost:${port}${baseUrl}`));
